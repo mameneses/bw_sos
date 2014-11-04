61f71d1289b74cc9012f0057720d479dba58a15a
Matias Meneses <matiasameneses@gmail.com>
2014-11-04 13:42:24 -0800

    added styling for phone numbers. Changed header for customer and product searches.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,11 +1,16 @@
 class Customer < ActiveRecord::Base
   has_many :orders, dependent: :destroy
   has_and_belongs_to_many :products
-  before_create :capitalize_names
+  before_create :capitalize_names, :style_phone_numbers
 
   private
     def capitalize_names
       self.first_name = self.first_name.capitalize
       self.last_name = self.last_name.capitalize
+    end
+
+    def style_phone_numbers
+      self.phone_num = self.phone_num.insert(0,"(").insert(4,")").insert(5," ").insert(9,"-")
+      self.alt_phone_num = self.alt_phone_num.insert(0,"(").insert(4,")").insert(5," ").insert(9,"-")
     end
 end
